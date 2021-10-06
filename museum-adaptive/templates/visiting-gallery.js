05e6f2d567784fa9afb47f88ea9f209f321a7ba8
Marina Tripetska <marina.tripetskaya@gmail.com>
2021-10-06 10:50:00 +0200

    fix: add PP on size 768px

--- museum-adaptive/templates/visiting-gallery.js
+++ museum-adaptive/templates/visiting-gallery.js
@@ -1,7 +1,7 @@
 export default Handlebars.compile(`{{#each this}}
 <li class="visiting__item">
     <a href={{link}} class="link card" target="_blanc" aria-label="link to google maps">
-        <img src={{img}} alt={{name}} width="440">
+        <img class="visiting__img" src={{img}} alt={{name}} width="440">
         <h3 class="card__name">{{name}}</h3>
         <p class="card__txt1">360° Virtual Tour</p>
         <p class="card__txt2">Google Street Panorama View</p>
