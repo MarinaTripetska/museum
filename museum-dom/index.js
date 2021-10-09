c1824e40081eece8b35e4bf3339892ae997f766b
Marina Tripetska <marina.tripetskaya@gmail.com>
2021-10-09 23:19:54 +0200

    feat: add slider in section explore

--- museum-dom/index.js
+++ museum-dom/index.js
@@ -3,6 +3,6 @@ import "./js/header-mobile-menu.js";
 import "./js/welcome-section-modal.js";
 import "./js/welcome-section-slider.js";
 import "./js/visiting-section-build-template.js";
-import "./js/explore.js"
+import "./js/explore.js";
 import "./js/video-section.js";
 import "./js/gallery.js";
--- museum-dom/js/explore.js
+++ museum-dom/js/explore.js
@@ -8,3 +8,17 @@
 //    const value = this.value;
 //     this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)` 
 // }
+
+//  oninput="beforeAfter()"
+//               onchange="beforeAfter()" 
+
+
+// function beforeAfter() {  
+//     document.querySelector('.explore__after')
+//         .style.width = document.getElementById('explore__input').value + "%";
+// }
+// document.querySelector('.explore__column-img')
+//     .addEventListener('oninput', beforeAfter);
+
+// document.querySelector('.explore__column-img')
+//     .addEventListener('onchange', beforeAfter)
